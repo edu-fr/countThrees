a3122463af2d1fbc97bb4c495940d433cf21785e
edu-fr <ra99571@uem.br>
2021-08-21 20:51:53 -0300

    changes on count_three_parallel

--- count_threes_parallel.c
+++ count_threes_parallel.c
@@ -1,14 +1,55 @@
 #include <stdio.h>
+#include <time.h> // used to start random seed
 #include <unistd.h> // getopt lib
 #include <stdlib.h> // exit_failure lib
-#include <time.h>
+#include <pthread.h> // thread lib
+#include <errno.h> // error number lib
+#include <math.h> // ceil lib
+
+
+#define handleErrorNumber(error_num, msg) \
+do { errno = error_num; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#define handleError(msg) \
+do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+struct thread_info {
+    pthread_t thread_id;
+    int thread_num;
+    int vector_size;
+    int initial;
+    int final;
+    int** vector;
+    int counter;
+};
+
+
+
+static void *threadStart(void *arg) {
+    struct thread_info *t_info = arg;
+
+    for (int i = t_info->initial; i < t_info->final; i++) {
+        if(t_info->vector[i] == 3) t_info->counter++;
+    }
+
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     int opt, vector_size, *vector, counter = 0;
+    int s, thread_num, num_threads;
+    struct thread_info *thread_info;
+    pthread_attr_t attr;
+
+
     while ((opt = getopt(argc, argv, "n:h:")) != -1) {
         switch(opt) {
             case 'n':
                 vector_size = strtoul(optarg, NULL, 0);
+                break;
+
+            case 't':
+                num_threads = strtoul(optarg, NULL, 0);
                 break;
 
             case 'h':
@@ -30,8 +71,24 @@ int main(int argc, char *argv[]) {
         vector[i] = rand() % 100;
     }
 
-    for (int i = 0; i < vector_size; i++) {
-        if(vector[i] == 3) counter++;
+    s = pthread_attr_init(&attr);
+    if (s != 0) {
+        handleErrorNumber(s, "pthread_attr_init");
+    }
+
+    thread_info = calloc(num_threads, sizeof(struct thread_info));
+    if(thread_info == NULL) {
+        handleError("thread_info calloc");
+    }
+
+    for (thread_num = 0; thread_num < num_threads; thread_num++) {
+        thread_info[thread_num].thread_num = thread_num + 1;
+        thread_info[thread_num].counter = 0;
+        thread_info[thread_num].initial = ceil(vector_size / num_threads) * thread_num; // PAREI AQUI
+        thread_info[thread_num].final = 0;
+        thread_info[thread_num].vector = 0;
+        thread_info[thread_num].vector_size = 0;
+
     }
 
     printf("Number of threes: %d", counter);
